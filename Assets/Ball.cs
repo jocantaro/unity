9b911ff00aaabd91f3666fbafb20899d5f426a3d
Jocantaro <ricardolopeztoledo@gmail.com>
2016-08-16 21:15:43 +0100

    first commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	int speedx;
+	int speedy;
+
+	float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (5, 10);
+
+		speedx= Random.Range (0, 2);
+
+		if (speedx == 0) {
+			speedx = 1;
+		} else {
+			speedx = -1;
+		}
+
+		speedy= Random.Range (0, 2);
+
+		if (speedy == 0) {
+			speedy = 1;
+		} else {
+			speedy = -1;
+		}
+
+		GetComponent<Rigidbody> ().velocity = new Vector3 (speed * speedx, speed * speedy, 0);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+
+	private float speed= 0.3f;
+
+	public KeyCode up;
+	public KeyCode down;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float direction = Input.GetAxis (this.name);
+		transform.Translate (0,speed*direction,0);
+
+		/*
+		if (Input.GetKey (up)) {
+			transform.Translate (0,speed,0);
+		}
+
+		if (Input.GetKey (down)) {
+			transform.Translate (0,-speed,0);
+		}
+		*/
+	}
+}
